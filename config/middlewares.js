a21b4bb52f6f53c5d12cbc0707a710e967782e89
KIM DONG CHOOL <dong@KIMs-MacBook-Pro.local>
2022-11-11 16:39:24 +0700

    add cloudinary config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,6 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,4 +9,19 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
